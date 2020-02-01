dac449d7ff3666e758261a06644fa5c4af206af8
Mark Dietzer <git@doridian.net>
2020-02-01 11:08:30 -0800

    diff image

--- agent/init.sh
+++ agent/init.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -euo pipefail
 
-cd /opt/hashtopolis-agent-python
+rm -rf /opt/agent
+mkdir -p /opt/agent
+cd /opt/agent
 
 BASE_URL="$1"
 API_KEY="$2"
 
 VOUCHER="$(curl -v "$BASE_URL/api/user.php" -H 'Content-Type: application/json' --data-binary "{\"section\":\"agent\",\"request\":\"createVoucher\",\"accessKey\":\"$API_KEY\"}" | jq -r .voucher)"
 
-exec python3 __main__.py  --voucher "$VOUCHER" --url "$BASE_URL/api/server.php"
+wget "$BASE_URL/agents.php?download=1" -O ./agent.zip
+
+exec python3 ./agent.zip  --voucher "$VOUCHER" --url "$BASE_URL/api/server.php"
